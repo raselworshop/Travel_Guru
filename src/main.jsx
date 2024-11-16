8d3206bb2af9cf979a74475ed61fda43aa8b7da2
raselworshop <raselworshop@gmail.com>
2024-11-16 14:49:16 +0800

    added router and context

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { RouterProvider } from 'react-router-dom'
+import route from './Routes/Route.jsx'
+import AuthProvider from './Provider/AuthProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <RouterProvider router={route}></RouterProvider>
+    </AuthProvider>
   </StrictMode>,
 )
